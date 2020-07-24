cb8d9a42acb6197a999a43a20de834f13c2f8d14
JekaPolihovic <polihovicjeka797@gmail.com>
2020-07-24 15:37:57 +0300

    Updates

---
+++ precache-manifest.210e47a6dbb00cf16a696be8784751aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943944ea3fbb102099f606c5e729c3b3",
+    "url": "/react-app-collaboration-basic-search/index.html"
+  },
+  {
+    "revision": "28a366eb2f8365c12a96",
+    "url": "/react-app-collaboration-basic-search/static/js/2.102e7c9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-collaboration-basic-search/static/js/2.102e7c9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e981ed1284b1747cb1bf",
+    "url": "/react-app-collaboration-basic-search/static/js/main.4d1b249d.chunk.js"
+  },
+  {
+    "revision": "58a95c0f02c5a21459d5",
+    "url": "/react-app-collaboration-basic-search/static/js/runtime-main.02b87f71.js"
+  }
+]);
